33349fbc758e35dfe4ceab9b50ae89b723f4fc1e
Brian Bautista <brianbauma10@gmail.com>
2023-10-18 19:27:16 +0200

    feat: :sparkles: List of Students

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,23 @@ import express, {type Express, type Request, type Response} from 'express';
 const app: Express = express();
 const port = process.env.HOST_PORT ?? '3000';
 
-const listaAPI = {
-	alumnes: ['Angulo Caballero, Alan Matthew', 'Argueta Navarrete, Nahomy Fernanda', 'Bautista Martin, Brian', 'Blay Estrada, Alex', 'Condal García, Pol',	Condal García, Pol	student
-	Seleccionar 'Cortes Comellas, Oriol'	Cortes Comellas, Oriol	editingteacher
-	Seleccionar 'Du Pre Peralta, Gerard'	Du Pre Peralta, Gerard	student
-	Seleccionar 'Gàndara Sánchez, Jaume'	Gàndara Sánchez, Jaume	student
-	Seleccionar 'Garrido Pintado, Albert'	Garrido Pintado, Albert	student
-	Seleccionar 'Herrerias Cuscullola, Rafel'	Herrerias Cuscullola, Rafel	student
-	Seleccionar 'Higueras Rubiño, Jaime'	Higueras Rubiño, Jaime	student
-	Seleccionar 'Linares Vigo, Joan'	Linares Vigo, Joan	student
-	Seleccionar 'Llamas Masdeu, Dídac'	Llamas Masdeu, Dídac	student
-	Seleccionar 'Muñoz Valenzuela, Alejandro'	Muñoz Valenzuela, Alejandro	student
-	Seleccionar 'Najarro García, Marc'	Najarro García, Marc	student
-	Seleccionar 'Pallarés Sirvent, Quim'	Pallarés Sirvent, Quim	student
-	Seleccionar 'Parra Raya, Óscar'	Parra Raya, Óscar	student
-	Seleccionar 'Pascual González, Alejandro'	Pascual González, Alejandro	student
-	Seleccionar 'Rodellar Martínez, Alberto'	Rodellar Martínez, Alberto	student
-	Seleccionar 'Ros Puig, Manuel'	Ros Puig, Manuel	student'],
-	ufs: []
-}
+const listaApi = {
+	alumnes: ['Angulo Caballero, Alan Matthew', 'Argueta Navarrete, Nahomy Fernanda', 'Bautista Martin, Brian', 'Blay Estrada, Alex', 'Condal García, Pol',	'Cortes Comellas, Oriol', 'Du Pre Peralta, Gerard',	'Gàndara Sánchez, Jaume', 'Garrido Pintado, Albert',	 'Herrerias Cuscullola, Rafel', 'Higueras Rubiño, Jaime', 'Linares Vigo, Joan',	 'Llamas Masdeu, Dídac', 'Muñoz Valenzuela, Alejandro',	'Najarro García, Marc',	'Pallarés Sirvent, Quim', 'Parra Raya, Óscar', 'Pascual González, Alejandro', 'Rodellar Martínez, Alberto', 'Ros Puig, Manuel', 'Sánchez Erazo, Luis Mario', 'Sans Lleixà, Martí', 'Sanz Sanchez, Oscar', 'Simón Pérez, Kevin', 'Soto Gómez, Eugeni', 'Trujillo Albert, Pol', 'Trujillo Guadiamos, Victor Manuel'],
+};
 
 // eslint-disable-next-line arrow-body-style
 app.get('/', (req: Request, res: Response) => {
 	return res.send({message: 'Hello World!'});
+});
+
+app.get('/alumnos', (req: Request, res: Response) => {
+	const nombres = listaApi.alumnes;
+	res.json({alumnos: nombres});
+});
+
+app.get('/total-alumnos', (req: Request, res: Response) => {
+	const totalAlumnos = listaApi.alumnes.length;
+	res.json({total: totalAlumnos});
 });
 
 app.listen(process.env.HOST_PORT, () => {
